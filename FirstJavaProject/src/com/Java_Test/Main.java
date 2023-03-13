74be946df23eb69e1ca4b68576335e7085c6d10d
MoyinDan <moyingirl.am@gmail.com>
2023-03-13 09:49:03 -0700

    Introduction to java (task 14)

---
+++ FirstJavaProject/src/com/Java_Test/Main.java
@@ -0,0 +1,22 @@
+package com.Java_Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first java project.");
+        System.out.println("My name is Mary Daniel.");
+        System.out.println("I am a software Tester.");
+        System.out.println("Mary Daniel is passionate about ensuring that software products are bug free.");
+        System.out.println("She has a background in software testing.");
+        System.out.println("She is skilled in creating and executing test plans.");
+        System.out.println("She has a strong ability to identifying defects.");
+        System.out.println("She works with developers to resolve issues.");
+        System.out.println("She has demonstrated an ability to adapt to different testing environments.");
+        System.out.println("She has experience working with a variety of software testing tools and methodologies.");
+        System.out.println("She is detail-oriented and methodical in her approach.");
+        System.out.println("She has proven track record of delivering high-quality software products on time.");
+        System.out.println("She is also an effective communicator and team player.");
+        System.out.println("She is committed to ensuring that software products are thoroughly tested.");
+        System.out.println("She meet the highest standards of quality.");
+    }
+}
